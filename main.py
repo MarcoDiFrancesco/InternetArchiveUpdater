62f0548818d4fe38c9aedaed0c29f4b4f6a3a907
MarcoDiFrancesco <marcodifran@gmail.com>
2020-08-07 22:05:20 +0200

    Update main

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from selenium import webdriver
+import socket
 import os
 
-CHROMEDRIVER_PATH = os.environ.get("CHROMEDRIVER_PATH")
-GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN")
+if socket.gethostname() == "bump":
+    CHROMEDRIVER_PATH = "/home/marco/Downloads/chromedriver"
+    GOOGLE_CHROME_BIN = "/usr/bin/google-chrome-stable"
+else:
+    CHROMEDRIVER_PATH = os.environ.get("CHROMEDRIVER_PATH")
+    GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN")
+
 
 opts = webdriver.ChromeOptions()
 opts.binary_location = GOOGLE_CHROME_BIN
@@ -11,5 +17,5 @@ opts.add_argument("--disable-dev-shm-usage")
 opts.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=opts)
 
-driver.get("https://www.google.com")
+driver.get("http://icanhazip.com/")
 print(driver.page_source)
